6a1173bbca203053dabe91092716d80602815903
Riyan <riyan0350@gmail.com>
2023-12-22 22:09:09 +0530

    added NoiseMapGenerator

---
+++ scripts/Global/NoiseMapGenerator.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+public class NoiseMapGenerator {
+
+	FastNoiseLite noise;
+
+
+	public NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum noiseEnum) {
+		noise = new FastNoiseLite();
+		noise.NoiseType = noiseEnum;
+	}
+
+
+	public float[,] Generate2DNoiseMap(int height, int width, float offsetX, float offsetY, float scale) {
+		float [,] noiseMap = new float[height, width];
+
+		for (int x = 0; x < height; x++) {
+			for (int y = 0; y < width; y++) {
+				noiseMap[x,y] = noise.GetNoise2D((x+offsetX)/scale, (y+offsetY)/scale);
+			}
+		}
+
+		return noiseMap;
+	}
+
+	public float[,] Generate2DNoiseMap(int height, int width, float scale) {
+		return Generate2DNoiseMap(height, width, 0, 0, scale);
+	}
+
+	public float[,] Generate2DNoiseMap(int height, int width) {
+		return Generate2DNoiseMap(height, width, 0, 0, 0.1F);
+	}
+}
