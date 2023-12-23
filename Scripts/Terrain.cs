b05c2081935083319da5e9e0052fbe0fa3a91841
Riyan <riyan0350@gmail.com>
2023-12-23 13:42:25 +0530

    Terrain.cs is able to compile

--- Scripts/Terrain.cs
+++ Scripts/Terrain.cs
@@ -1,20 +1,42 @@
 using Godot;
 using Global;
+using System.Data;
 
 
+[Tool]
 public partial class Terrain : MeshInstance3D
 {
+
+	[Export]
+	private int NoiseRows = 10;
+	[Export]
+	private int NoiseColumns = 10;
+	[Export]
+	private float NoiseScale = 0.1F;
+	[Export]
+	private bool AutoUpdate = false;
+	[Export]
+	private float CellWidth = 1F;
+	[Export]
+	private float HeightLimit = 20F;
+
+
 	public override void _Ready() {
 		generateTerrain();
 	}
 
-	private void generateTerrain() {
-		NoiseMapGenerator NMG = new NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum.Perlin);
 
-		Vector3[] vertices = new Vector3[3];
-		vertices[0] = new Vector3(0, 1, 0);
-		vertices[1] = new Vector3(1, 0, 0);
-		vertices[2] = new Vector3(0, 0, 1);
+	private void generateTerrain() {
+		NoiseMapGenerator Noise = new NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum.Perlin);
+		float [,] noiseMap = Noise.Generate2DNoiseMap(NoiseRows, NoiseColumns, NoiseScale);
+
+		Vector3[] vertices = new Vector3[(NoiseRows-1)*(NoiseColumns-1)*6];
+		int currentIndex = 0;
+		for (int x = 0; x < NoiseRows; x++) {
+			for (int z = 0; z < NoiseColumns; z++) {
+				vertices[currentIndex++] = new Vector3(x*CellWidth, noiseMap[x,z]*HeightLimit, z*CellWidth);
+			}
+		}
 
 		ArrayMesh arrayMesh = new ArrayMesh();
 		Godot.Collections.Array array = new Godot.Collections.Array();
