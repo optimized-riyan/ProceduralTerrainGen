9f4ec976cd31b9c37c46b46826644dfdbec806ce
Riyan <riyan0350@gmail.com>
2023-12-23 11:56:25 +0530

    logic for generateTerrain()

--- Scripts/Global/NoiseMapGenerator.cs
+++ Scripts/Global/NoiseMapGenerator.cs
@@ -1,33 +1,43 @@
 using Godot;
 
-public class NoiseMapGenerator {
+namespace Global {
+	public class NoiseMapGenerator {
 
-	FastNoiseLite noise;
-
-
-	public NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum noiseEnum) {
-		noise = new FastNoiseLite();
-		noise.NoiseType = noiseEnum;
-	}
+		FastNoiseLite noise;
 
 
-	public float[,] Generate2DNoiseMap(int height, int width, float offsetX, float offsetY, float scale) {
-		float [,] noiseMap = new float[height, width];
+		public NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum noiseEnum) {
+            noise = new FastNoiseLite {
+                NoiseType = noiseEnum
+            };
+        }
 
-		for (int x = 0; x < height; x++) {
-			for (int y = 0; y < width; y++) {
-				noiseMap[x,y] = noise.GetNoise2D((x+offsetX)/scale, (y+offsetY)/scale);
+
+		public NoiseMapGenerator() {
+            noise = new FastNoiseLite {
+                NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin
+            };
+        }
+
+
+		public float[,] Generate2DNoiseMap(int height, int width, float offsetX, float offsetY, float scale) {
+			float [,] noiseMap = new float[height, width];
+
+			for (int x = 0; x < height; x++) {
+				for (int y = 0; y < width; y++) {
+					noiseMap[x,y] = noise.GetNoise2D((x+offsetX)/scale, (y+offsetY)/scale);
+				}
 			}
+
+			return noiseMap;
 		}
 
-		return noiseMap;
-	}
+		public float[,] Generate2DNoiseMap(int height, int width, float scale) {
+			return Generate2DNoiseMap(height, width, 0, 0, scale);
+		}
 
-	public float[,] Generate2DNoiseMap(int height, int width, float scale) {
-		return Generate2DNoiseMap(height, width, 0, 0, scale);
-	}
-
-	public float[,] Generate2DNoiseMap(int height, int width) {
-		return Generate2DNoiseMap(height, width, 0, 0, 0.1F);
+		public float[,] Generate2DNoiseMap(int height, int width) {
+			return Generate2DNoiseMap(height, width, 0, 0, 0.1F);
+		}
 	}
 }
--- Scripts/Terrain.cs
+++ Scripts/Terrain.cs
@@ -1,12 +1,28 @@
 using Godot;
+using Global;
+
 
 public partial class Terrain : MeshInstance3D
 {
 	public override void _Ready() {
-
+		generateTerrain();
 	}
 
 	private void generateTerrain() {
-		
+		NoiseMapGenerator NMG = new NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum.Perlin);
+
+		Vector3[] vertices = new Vector3[3];
+		vertices[0] = new Vector3(0, 1, 0);
+		vertices[1] = new Vector3(1, 0, 0);
+		vertices[2] = new Vector3(0, 0, 1);
+
+		ArrayMesh arrayMesh = new ArrayMesh();
+		Godot.Collections.Array array = new Godot.Collections.Array();
+		array.Resize((int) Mesh.ArrayType.Max);
+		array[(int)Mesh.ArrayType.Vertex] = vertices;
+
+		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, array);
+
+		this.Mesh = arrayMesh;
 	}
 }
