d84868a5db2a3e69724597dc3c8365469a9412e1
Riyan <riyan0350@gmail.com>
2023-12-24 16:48:01 +0530

    before adding index array, will have to change considerable amount of the program

--- Scripts/Terrain.cs
+++ Scripts/Terrain.cs
@@ -17,7 +17,7 @@ public partial class Terrain : MeshInstance3D
 	[Export]
 	private float CellWidth = 1F;
 	[Export]
-	private float HeightLimit = 20F;
+	private float HeightLimit = 10F;
 
 
 	public override void _Ready() {
@@ -26,20 +26,41 @@ public partial class Terrain : MeshInstance3D
 
 
 	private void generateTerrain() {
-		NoiseMapGenerator Noise = new NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum.Perlin, 0F, 1F);
+		NoiseMapGenerator Noise = new NoiseMapGenerator(FastNoiseLite.NoiseTypeEnum.SimplexSmooth, 0F, 1F);
 		float [,] noiseMap = Noise.Generate2DNoiseMap(NoiseRows, NoiseColumns, NoiseScale);
 
 		Vector3[] vertices = new Vector3[(NoiseRows-1)*(NoiseColumns-1)*6];
-		int currentIndex = 0;
+		int vertIndex = 0;
+		float lengthB2 = NoiseRows*CellWidth/2;
+		float widthB2 = NoiseColumns*CellWidth/2;
+
+		Vector2[] uvs = new Vector2[(NoiseRows-1)*(NoiseColumns-1)*6];
+		int uvIndex = 0;
+
+		Vector3[] normals = new Vector3[(NoiseRows-1)*(NoiseColumns-1)*6];
+		int normalIndex = 0;
+
 		for (int x = 0; x < NoiseRows-1; x++) {
 			for (int z = 0; z < NoiseColumns-1; z++) {
-				vertices[currentIndex++] = new Vector3(x*CellWidth, noiseMap[x,z]*HeightLimit, z*CellWidth);
-				vertices[currentIndex++] = new Vector3((x+1)*CellWidth, noiseMap[x+1,z+1]*HeightLimit, (z+1)*CellWidth);
-				vertices[currentIndex++] = new Vector3(x*CellWidth, noiseMap[x,z+1]*HeightLimit, (z+1)*CellWidth);
+				// triangle 1
+				vertices[vertIndex++] = new Vector3(x*CellWidth-lengthB2, noiseMap[x,z]*HeightLimit, z*CellWidth-widthB2);
+				vertices[vertIndex++] = new Vector3((x+1)*CellWidth-lengthB2, noiseMap[x+1,z+1]*HeightLimit, (z+1)*CellWidth-widthB2);
+				vertices[vertIndex++] = new Vector3(x*CellWidth-lengthB2, noiseMap[x,z+1]*HeightLimit, (z+1)*CellWidth-widthB2);
 
-				vertices[currentIndex++] = new Vector3(x*CellWidth, noiseMap[x,z]*HeightLimit, z*CellWidth);
-				vertices[currentIndex++] = new Vector3((x+1)*CellWidth, noiseMap[x+1,z]*HeightLimit, z*CellWidth);
-				vertices[currentIndex++] = new Vector3((x+1)*CellWidth, noiseMap[x+1,z+1]*HeightLimit, (z+1)*CellWidth);
+				// triangle 2
+				vertices[vertIndex++] = new Vector3(x*CellWidth-lengthB2, noiseMap[x,z]*HeightLimit, z*CellWidth-widthB2);
+				vertices[vertIndex++] = new Vector3((x+1)*CellWidth-lengthB2, noiseMap[x+1,z]*HeightLimit, z*CellWidth-widthB2);
+				vertices[vertIndex++] = new Vector3((x+1)*CellWidth-lengthB2, noiseMap[x+1,z+1]*HeightLimit, (z+1)*CellWidth-widthB2);
+
+				// uv of tri 1, using z as y
+				uvs[uvIndex++] = new Vector2(x/NoiseRows, z/NoiseColumns);
+				uvs[uvIndex++] = new Vector2((x+1)/NoiseRows, (z+1)/NoiseColumns);
+				uvs[uvIndex++] = new Vector2(x/NoiseRows, (z+1)/NoiseColumns);
+
+				// uv of tri 2, again using z as y
+				uvs[uvIndex++] = new Vector2(x/NoiseRows, z/NoiseColumns);
+				uvs[uvIndex++] = new Vector2((x+1)/NoiseRows, z/NoiseColumns);
+				uvs[uvIndex++] = new Vector2((x+1)/NoiseRows, (z+1)/NoiseColumns);
 			}
 		}
 
@@ -47,6 +68,7 @@ public partial class Terrain : MeshInstance3D
 		Godot.Collections.Array array = new Godot.Collections.Array();
 		array.Resize((int) Mesh.ArrayType.Max);
 		array[(int)Mesh.ArrayType.Vertex] = vertices;
+		array[(int)Mesh.ArrayType.TexUV] = uvs;
 
 		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, array);
 
