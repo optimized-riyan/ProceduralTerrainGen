1bbac85f21067c27715bfda6ddf66a614b946515
Riyan <riyan0350@gmail.com>
2023-12-28 13:22:39 +0530

    added logic to resolve normals on edges, not working tho, gotta fix it after implementing chunking

--- Scripts/Terrain.cs
+++ Scripts/Terrain.cs
@@ -122,28 +122,69 @@ public partial class Terrain : MeshInstance3D
 			}
 		}
 
+		// edge vertices normals
+		Vector3[] top = new Vector3[NoiseRows];
+		Vector3[] bottom = new Vector3[NoiseRows];
+		Vector3[] left = new Vector3[NoiseColumns];
+		Vector3[] right = new Vector3[NoiseColumns];
+
+		for (int j = 0; j < NoiseRows; j++) {
+			top[j] = new Vector3(j*CellWidth, NMG.GetNoiseAt(-1, j, NoiseScale)*HeightLimit, -1*CellWidth);
+			bottom[j] = new Vector3(j*CellWidth, NMG.GetNoiseAt(NoiseColumns, j, NoiseScale)*HeightLimit, NoiseColumns*CellWidth);
+		}
+		for (int i = 0; i < NoiseColumns; i++) {
+			left[i] = new Vector3(-1*CellWidth, NMG.GetNoiseAt(i, -1, NoiseScale)*HeightLimit, i*CellWidth);
+			right[i] = new Vector3(NoiseRows*CellWidth, NMG.GetNoiseAt(i, NoiseRows, NoiseScale), i*CellWidth);
+		}
+
+		// top right and bottom left can be ignored since both won't be used
+		Vector3 topLeft, bottomRight;
+		topLeft = new Vector3(-1*CellWidth, NMG.GetNoiseAt(-1, -1, NoiseScale)*HeightLimit, -1*CellWidth);
+		bottomRight = new Vector3(NoiseColumns*CellWidth, NMG.GetNoiseAt(NoiseColumns, NoiseRows, NoiseScale)*HeightLimit, NoiseRows*CellWidth);
+
+		// top and bottom
+		for (int j = 0; j < NoiseRows-1; j++) {
+			triSurfaceNormal = calculateSurfaceNormal(top[j], vertices[j*NoiseColumns], vertices[(j+1)*NoiseColumns]);
+			normals[j*NoiseColumns] += triSurfaceNormal;
+			normals[(j+1)*NoiseColumns] += triSurfaceNormal;
+			triSurfaceNormal = calculateSurfaceNormal(top[j], vertices[(j+1)*NoiseColumns], top[j+1]);
+			normals[(j+1)*NoiseColumns] += triSurfaceNormal;
+
+			triSurfaceNormal = calculateSurfaceNormal(vertices[NoiseColumns-1 + j*NoiseColumns], bottom[j+1], vertices[NoiseColumns-1 + (j+1)*NoiseColumns]);
+			normals[NoiseColumns-1 + j*NoiseColumns] += triSurfaceNormal;
+			normals[NoiseColumns-1 + (j+1)*NoiseColumns] += triSurfaceNormal;
+			triSurfaceNormal = calculateSurfaceNormal(vertices[NoiseColumns-1 + j*NoiseColumns], bottom[j], bottom[j+1]);
+			normals[NoiseColumns-1 + j*NoiseColumns] += triSurfaceNormal;
+		}
+
+		// left and right
+		for (int i = 0; i < NoiseColumns-1; i++) {
+			triSurfaceNormal = calculateSurfaceNormal(left[i], vertices[i+1], vertices[i]);
+			normals[i+1] += triSurfaceNormal;
+			normals[i] += triSurfaceNormal;
+			triSurfaceNormal = calculateSurfaceNormal(left[i], left[i+1], vertices[i+1]);
+			normals[i+1] += triSurfaceNormal;
+
+			triSurfaceNormal = calculateSurfaceNormal(vertices[i + (NoiseRows-1)*NoiseColumns], vertices[i+1 + (NoiseRows-1)*NoiseColumns], right[i+1]);
+			normals[i + (NoiseRows-1)*NoiseColumns] += triSurfaceNormal;
+			normals[i+1 + (NoiseRows-1)*NoiseColumns] += triSurfaceNormal;
+			triSurfaceNormal = calculateSurfaceNormal(right[i], vertices[i+1 + (NoiseRows-1)*NoiseColumns], right[i+1]);
+			normals[i+1 + (NoiseRows-1)*NoiseColumns] += triSurfaceNormal;
+		}
+
+		normals[0] += calculateSurfaceNormal(topLeft, vertices[0], top[0]);
+		normals[0] += calculateSurfaceNormal(topLeft, left[0], vertices[0]);
+		normals[NoiseRows*NoiseColumns-1] += calculateSurfaceNormal(vertices[NoiseRows*NoiseColumns - 1], bottomRight, right[NoiseColumns-1]);
+		normals[NoiseRows*NoiseColumns-1] += calculateSurfaceNormal(vertices[NoiseRows*NoiseColumns-1], bottom[NoiseRows-1], bottomRight);
+		normals[NoiseColumns-1] += calculateSurfaceNormal(vertices[NoiseColumns-1], left[NoiseColumns-1], bottom[0]);
+		normals[(NoiseRows-1)*NoiseColumns] += calculateSurfaceNormal(top[NoiseRows-1], vertices[(NoiseRows-1)*NoiseColumns], right[0]);
+
 		// normalize all vertex normals
 		for (int i = 0; i < NoiseColumns; i++) {
 			for (int j = 0; j < NoiseRows; j++) {
 				normals[i + j*NoiseColumns] = normals[i + j*NoiseColumns].Normalized();
 			}
 		}
-
-		// edge vertices normals
-		float[] top = new float[NoiseRows];
-		float[] bottom = new float[NoiseRows];
-		float[] left = new float[NoiseColumns];
-		float[] right = new float[NoiseColumns];
-
-		for (int j = 0; j < NoiseRows; j++) {
-			top[j] = NMG.GetNoiseAt(0, j, NoiseScale);
-			bottom[j] = NMG.GetNoiseAt(NoiseColumns-1, j, NoiseScale);
-		}
-		for (int i = 0; i < NoiseColumns; i++) {
-			left[i] = NMG.GetNoiseAt(i, 0, NoiseScale);
-			right[i] = NMG.GetNoiseAt(i, NoiseRows-1, NoiseScale);
-		}
-
 
 		ArrayMesh arrayMesh = new ArrayMesh();
 		Godot.Collections.Array array = new Godot.Collections.Array();
